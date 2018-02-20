b4722485ef2ed74aa9c86785dee365fa7649e383
Aksman007 <akshaygawari2@gmail.com>
2018-02-20 16:11:13 +0530

    mail service

--- InvoicingRepository/UnitOfWork.cs
+++ InvoicingRepository/UnitOfWork.cs
@@ -40,5 +40,30 @@ namespace InvoicingRepository
                 return _mailRepository = _mailRepository ?? new GenericRepository<Mail>(_invoicingContext);
             }
         }
+
+        public void Save()
+        {
+            _invoicingContext.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _invoicingContext.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
     }
 }
--- InvoicingService/MailService.cs
+++ InvoicingService/MailService.cs
@@ -1,9 +1,19 @@
 ﻿using System;
+using System.Linq;
+using EFCoreTutorialGit;
+using InvoicingRepository;
 
 namespace InvoicingService
 {
-    public class MailService
+    public class MailService : IMailService
     {
-
+        private static InvoicingToolContext invoicingContext = new InvoicingToolContext();
+        UnitOfWork uow = new UnitOfWork(invoicingContext);
+        public Mail GetMailByScheduleId(int id)
+        {
+            return uow.MailRepository.GetAll()
+                .Where(a => a.InvoiceSchedId == id)
+                .FirstOrDefault();         
+        }
     }
 }
